3d8bc781a2444a593020f6be954f768e4c7f2d85
Scott M <gmat191@gmail.com>
2018-09-17 03:20:59 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -14,5 +14,6 @@ PseudoCode
 prime method takes a number as an argument
 has a condition that checks if a number is prime or even 
 number is either greater than or equal to 2 
-range of 2-all numbers
+range starts with the 3rd element of the array all the way to the last element
+all? checks every value in our array for prime and even numbers
 =end
