1028f19f1ce829626c8a4edc695370387019a517
Scott M <gmat191@gmail.com>
2018-09-17 03:21:45 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -16,4 +16,5 @@ has a condition that checks if a number is prime or even
 number is either greater than or equal to 2 
 range starts with the 3rd element of the array all the way to the last element
 all? checks every value in our array for prime and even numbers
+if a number has a remainder it is prime
 =end
