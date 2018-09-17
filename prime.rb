47937689bb74411962b098ab79032cb957dd2cb9
Scott M <gmat191@gmail.com>
2018-09-17 03:02:17 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,6 @@ require 'prime'
 
 prime_array = Prime.take_while { |p| p < 2,000,000 }
 
-update_array = prime_array.inject { |sum, x| sum + x }
+total_count = prime_array.inject { |sum, x| sum + x }
 
-p updated_array
+p total_count
